f7f74aa22ca769aa2b75443cf458d8264b23016b
menna <menna.mohamed.omar@gmail.com>
2021-06-16 00:14:09 +0200

    edit maintenance page

--- src/components/layout/maintenance.jsx
+++ src/components/layout/maintenance.jsx
@@ -8,6 +8,7 @@ const Maintenance = () => {
         <div className="maintenance__container">
             <div className="maintenance__container__header">
                 <img className="maintenance__container__header__img" src="/images/logo02.png" alt=""/>
+                <h5 className="maintenance__container__header__h5">لاستيراد المعدات اليدوية والادوات الكهربية</h5>
             </div>
             <div className="maintenance__container__image">
                 <img className="maintenance__container__image__img" src="/images/Underconstruction.gif" alt=""/>
