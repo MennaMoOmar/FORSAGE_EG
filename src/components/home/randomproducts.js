7c83228ce2f499351ede11734cf6009d127b092e
menna <menna.mohamed.omar@gmail.com>
2021-06-21 23:41:39 +0200

    add prodict card

--- src/components/home/randomproducts.js
+++ src/components/home/randomproducts.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import ProductCard from '../shared/productcard';
+
 const Brands = () => {
 	return (
 		<React.Fragment>
@@ -10,6 +12,20 @@ const Brands = () => {
 							WCM قائمة أدوات <i class="fas fa-chevron-left"></i>
 						</h4>
 						<p className="seeall">عرض الكل</p>
+					</div>
+					<div className="row">
+						<div className="col-6 col-lg-3">
+							<ProductCard></ProductCard>
+						</div>
+						<div className="col-6 col-lg-3">
+							<ProductCard></ProductCard>
+						</div>
+						<div className="col-6 col-lg-3">
+							<ProductCard></ProductCard>
+						</div>
+						<div className="col-6 col-lg-3">
+							<ProductCard></ProductCard>
+						</div>
 					</div>
 				</div>
 			</div>
--- src/components/shared/productcard.js
+++ src/components/shared/productcard.js
@@ -5,7 +5,19 @@ const ProductCard = () => {
 		<React.Fragment>
 			<div className="productcard">
 				<div className="container">
-                </div>
+					<div className="productcard__image">
+						<img src="./images/product1.png" alt="product"></img>
+					</div>
+
+					<div className="productcard__data">
+						<p>طقم أدوات 110 قطعة</p>
+						<p>الكود 123</p>
+					</div>
+
+					<div className="productcard__button">
+						<button className="button mainbtn">عرض التفاصيل</button>
+					</div>
+				</div>
 			</div>
 		</React.Fragment>
 	);
