307647be66e13a5f78c5c37085ad6961461995a9
menna <menna.mohamed.omar@gmail.com>
2021-06-22 03:16:00 +0200

    add login page

--- src/components/layout/login.js
+++ src/components/layout/login.js
@@ -1,12 +1,36 @@
-import React from "react";
-import Button from "@material-ui/core/Button";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+
+const useStyles = makeStyles((theme) => ({
+	root: {
+		'& > *': {
+			margin: theme.spacing(1),
+			width: '25ch',
+		},
+	},
+}));
 
 const Login = () => {
-  return (
-    <div>
-      <Button variant="contained">Default</Button>
-    </div>
-  );
+	const classes = useStyles();
+	return (
+		<React.Fragment>
+			<div className="login">
+      <img src="/images/logo.png" alt=""></img>
+				<form className={classes.root} noValidate autoComplete="off">
+					<TextField id="standard-basic" label="اسم المستخدم" className="login__inputform" />
+					<TextField
+						id="standard-password-input"
+						label="كلمة المرور"
+						type="password"
+						autoComplete="current-password"
+            className="login__inputform"
+					/>
+					<button className="button mainbtn">تسجيل الدخول</button>
+				</form>
+			</div>
+		</React.Fragment>
+	);
 };
 
 export default Login;
