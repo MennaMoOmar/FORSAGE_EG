7d2833f7b149e3b902e19205b0480d66c683fd34
menna <menna.mohamed.omar@gmail.com>
2021-07-09 18:37:59 +0200

    add contact info to footer

--- src/components/shared/footer.js
+++ src/components/shared/footer.js
@@ -1,27 +1,33 @@
-import React from 'react';
+import React from 'react'
 
 const Footer = () => {
-	return (
-		<>
-			<div className="footer">
-				<div className="container">
-					<div className="row">
-						<div className="col-4 footer__links">
-							<h5>تواصل معنا</h5>
-						</div>
-						<div className="col-4 footer__content">
-							<p>الصفحة الرئيسية</p>
-							<p>الشركات</p>
-							<p>عن الموقع</p>
-						</div>
-						<div className="col-4 footer__logo">
-							<img src="/images/Logofooter.png" alt=""></img>
-						</div>
-					</div>
-				</div>
-			</div>
-		</>
-	);
-};
+  return (
+    <>
+      <div className='footer'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-4 footer__links'>
+              <h5>تواصل معنا</h5>
+              <ul>
+                <li>٠١٠٠٢٤٢٣٢٩١</li>
+                <li>٠١٥٥٤٤٠٣٦٠١</li>
+                <li>٠١٥٥٤٤٠٣٦٠٢</li>
+                <li>٠١٥٥٤٤٠٣٦٠٣</li>
+              </ul>
+            </div>
+            <div className='col-4 footer__content'>
+              <p>الصفحة الرئيسية</p>
+              <p>الشركات</p>
+              <p>عن الموقع</p>
+            </div>
+            <div className='col-4 footer__logo'>
+              <img src='/images/Logofooter.png' alt=''></img>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
 
-export default Footer;
+export default Footer
